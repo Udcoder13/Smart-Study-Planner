e9191a409984fe0969627334a0277e3536a2e294
Udit Yadav <udy2580y@gmail.com>
2025-12-17 01:52:07 +0530

    deployment change

--- project/client/src/api.js
+++ project/client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'  // Your backend server
+  baseURL: import.meta.env.VITE_API_BASE_URL,  // Your backend server
 });
 
 api.interceptors.request.use(config => {
