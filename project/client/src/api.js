eb7da59a84c8fc541925bf37243871e267a6a604
Udit Yadav <udy2580y@gmail.com>
2025-12-18 02:22:06 +0530

    integrating nginx

--- project/client/src/api.js
+++ project/client/src/api.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,  // Your backend server
+  baseURL: '/api',
 });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-    console.log('[API] Sending token in header:', token);
-  } else {
-    console.warn('[API] No token found in localStorage');
   }
   return config;
 });
